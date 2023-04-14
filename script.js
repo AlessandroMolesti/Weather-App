6119e90bf776394d51b9bcf259392474be4febf4
Mole <ale.molesti@gmail.com>
2023-04-14 15:15:51 +0200

    Home page structuring

--- script.js
+++ script.js
@@ -41,3 +41,28 @@ btn.addEventListener('click', function(){
   .catch(err => alert('You entered Wrong city name'))
 })
 //If you click on the submit button without inputting anything in the input box or typing the wrong city name then the above text can be seen.
+
+
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
